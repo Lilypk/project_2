f8d0d56ff7aaf87a8c2394279a7a42825beb5b62
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-02 20:41:47 -0400

    first try at creating the deck class

--- game.js
+++ game.js
@@ -1,19 +1,26 @@
-
-
+class Card {
+    constructor(suits, cardNames, cardValues) {
+        this.suits = suit;
+        this.cardNames = rank;
+        this.cardValues = score;
+    }
+}
 class Deck {
     constructor(length) {
-    this.length = 52
+        this.length = 52
     }
-    makeDeck () {
+    makeDeck() {
         let deck = []
-        let suits = ('hearts', 'spades', 'clubs', 'diamonds')
-        let rank = ('Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", 'Queen', 'King')
-        let score = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
-        for (let i=0; i < suit.length; i++){
-            for (let j=0; j < rank.length; j++){
-            return card;
-    }
-    }
+        let suits = ['hearts', 'spades', 'clubs', 'diamonds']
+        let cardNames = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
+        let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ]
+        for (let i = 0; i < suits.length; i++) {
+            for (let j = 0; j < cardNames.length; j++) {
+                let card = {
+
+                }
+            }
+        }
     }
 
 }
