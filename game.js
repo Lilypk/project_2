15851593df8f9096737a86d909ded6475ee3440f
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-02 20:24:39 -0400

    first commit

---
+++ game.js
@@ -0,0 +1,20 @@
+
+
+class Deck {
+    constructor(length) {
+    this.length = 52
+    }
+    makeDeck () {
+        let deck = []
+        let suits = ('hearts', 'spades', 'clubs', 'diamonds')
+        let rank = ('Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", 'Queen', 'King')
+        let score = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
+        for (let i=0; i < suit.length; i++){
+            for (let j=0; j < rank.length; j++){
+            return card;
+    }
+    }
+    }
+
+}
+
