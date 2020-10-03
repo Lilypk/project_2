26a602e128d1a175c0d66f8ec1f42eb5c42d6cbf
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-02 22:39:58 -0400

    re-do of card class

--- game.js
+++ game.js
@@ -1,28 +1,31 @@
 class Card {
-    constructor(suits, cardNames, cardValues) {
-        this.suits = suit;
-        this.cardNames = rank;
-        this.cardValues = score;
-    }
-}
-class Deck {
-    constructor(length) {
-        this.length = 52
-    }
-    makeDeck() {
-        let deck = []
-        let suits = ['hearts', 'spades', 'clubs', 'diamonds']
-        let cardNames = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
-        let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ]
-        for (let i = 0; i < suits.length; i++) {
-            for (let j = 0; j < cardNames.length; j++) {
-                return card
-            }
+        constructor(suits, cardNames, cardValues) {
+            this.suits = suit;
+            this.cardNames = rank;
+            this.cardValues = score;
         }
     }
 
-}
-class Players{
-}
-class Game{
-}
+    class Deck {
+            constructor() {
+                this.deck = []
+            }
+            makeDeck() {
+                
+                let suits = ['hearts', 'spades', 'clubs', 'diamonds']
+                let cardNames = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
+                let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ]
+                for (let i = 0; i < suits.length; i++) {
+                    for (let j = 0; j < cardNames.length; j++) {
+                        let card = {
+                            suit:suits[i],
+                            name:cardNames[j],
+                            values:cardValues[j]
+                        }
+                    }
+                }
+            }
+    }
+    const deckReady = new Deck()
+    console.log(deckReady.deck)
+
