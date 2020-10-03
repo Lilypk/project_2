39711d5b5fca4257233b7477237f9af52c01604b
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-03 10:10:10 -0400

    added to readme

--- game.js
+++ game.js
@@ -1,31 +1,37 @@
 class Card {
-        constructor(suits, cardNames, cardValues) {
+        constructor(suits, cardValues) {
             this.suits = suit;
-            this.cardNames = rank;
-            this.cardValues = score;
+            this.cardValues = value;
         }
     }
 
-    class Deck {
+class Deck {
             constructor() {
                 this.deck = []
             }
             makeDeck() {
-                
                 let suits = ['hearts', 'spades', 'clubs', 'diamonds']
                 let cardValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
                 for (let i = 0; i < suits.length; i++) {
                     for (let j = 0; j < cardValues.length; j++) {
-                        var card = {
-                           value: cardValues[j],
-                           suit: suits[i]
+                        let card = {
+                           suit:suits[i],
+                           value:cardValues[j]
                            
                         }
+                        deck.push(card)
                     }
-                    return deck
+                    
                 }
+                return deck
             }
+            
     }
-    const deckReady = new Deck()
+    const deckReady = new Deck
     console.log(deckReady.deck)
-
+    
+    
+class Players {
+    }
+class Game {
+    }
