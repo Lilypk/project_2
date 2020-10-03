cbc80e0148145fe3baa7a76824f22c8b19d4ca44
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-03 15:09:39 -0400

    added shuffle()

--- game.js
+++ game.js
@@ -1,46 +1,61 @@
 class Card {
-        constructor(suits, cardValues) {
-            this.suits = suits;
-            this.cardValues = cardValues;
-            
-        }
+    constructor(suits, cardValues) {
+        this.suits = suits;
+        this.cardValues = cardValues;
+
     }
+}
 
 class Deck {
-            constructor() {
-                this.deck = []
-                this.makeDeck()
-            }
-            makeDeck() {
-                let suits = ['hearts', 'spades', 'clubs', 'diamonds']
-                let cardValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-                
-                for (let i = 0; i < suits.length; i++) {
-                    for (let j = 0; j < cardValues.length; j++) {
-                      
-                        let card = {
-                           suit:suits[i],
-                           value:cardValues[j],
-                        
-                        }
-                        
-                        this.deck.push(card)
-                    }
-                    
+    constructor() {
+        this.deck = []
+        this.makeDeck()
+        
+    }
+    makeDeck() {
+        let suits = ['hearts', 'spades', 'clubs', 'diamonds']
+        let cardValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+        for (let i = 0; i < suits.length; i++) {
+            for (let j = 0; j < cardValues.length; j++) {
+
+                let card = {
+                    suit: suits[i],
+                    value: cardValues[j],
+
                 }
-                
-               
+
+                this.deck.push(card)
             }
-               
+
+        }
+
 
     }
+    shuffle(array) {
+            var currentIndex = array.length, temporaryValue, randomIndex;
+        
+        
+            while (0 !== currentIndex) {
+        
+        
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+        
+        
+                temporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+            }
+            return array;
+        }
     
-  
-    let deckReady = new Deck
-    console.log(deckReady)
-    
-    
-class Players {
-    }
-class Game {
-    }
+}
+
+let deckReady = new Deck
+console.log(deckReady)
+
+
+
+
+
