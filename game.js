e2ecb7605efc996dd7d3d60ec9f56f3a3d7457d3
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-04 18:13:22 -0400

    6:15, added some things to war

--- game.js
+++ game.js
@@ -6,15 +6,12 @@ class Card {
     printCard() {
         console.log(`${this.name} ${this.cardValues}`)
     }
-
 }
 class Deck {
     constructor() {
         this.deck = []
         this.makeDeck()
         this.shuffleDeck(this.deck)
-
-
     }
     makeDeck() {
         let suits = ['hearts', 'spades', 'clubs', 'diamonds']
@@ -26,15 +23,10 @@ class Deck {
                 let card = {
                     suit: suits[i],
                     value: cardValues[j],
-
                 }
-
                 this.deck.push(card)
             }
-
         }
-
-
     }
     shuffleDeck(array) {
         var currentIndex = array.length, temporaryValue, randomIndex;
@@ -59,29 +51,26 @@ class Deck {
         let playerTwoDeck = deck.splice(-half)
         return [playerOneDeck, playerTwoDeck]
     }
-
 }
 class Player {
     constructor(name, playerDeck) {
         this.name = []
         this.deck = playerDeck
-        
     }
     getRemainingCards() {
-        //how many cards do they have left
+        //how many cards do the players have left
     }
     playCard() {
         let cardInPlay = this.deck.pop()
         return cardInPlay
     }
     giveThreeCards() {
-        //three cards are drawn out of each players deck for war
+        let warCards = this.deck.splice(this.deck.length - 3, 3)
+        return warCards
     }
     receiveCardsForWinning(winningCards) {
         this.deck.unshift(...winningCards)
-        
     }
-
 }
 class Game {
     constructor(playerOneName, playerTwoName) {
@@ -90,9 +79,10 @@ class Game {
         this.playerOne = new Player(playerOneName, deckHalves[0])
         this.playerTwo = new Player(playerTwoName, deckHalves[1])
         this.conductTurn()
+        
     }
     conductTurn() {
-        let cardStack = [this.playerOne.deck, this.playerTwo.deck]
+        //get playCard from players 
     }
     compareCards() {
         let p1Card = this.playerOne.cardInPlay
@@ -104,15 +94,18 @@ class Game {
         } else {
             this.declareWar()
         }
-
     }
     giveWinnerCards(winnerName, winnerCards) {
         this[winnerName].receiveCardsForWinning[winnerCards]
-        if(this.playerOne.deck.length == 52 || this.playerTwo.deck.length == 52) {
+        if(this.playerOne.deck.length == 0 || this.playerTwo.deck.length == 0) {
             this.gameEnd()
         }
     }
     declareWar() {
+        let p1WarCards = this.playerOne.warCards
+        let p2WarCards = this.playerTwo.warCards
+
+        
         //each player giveThreeCards
         //fourth card compareCards
         //if one is greater, giveWinnerCards
@@ -122,12 +115,8 @@ class Game {
     gameEnd(winnerName) {
         console.log(`The winner of the game is ${this[winnerName].name}!`)
     }
-    printWinnerName() {
-        
-    }
+    
 }
-
-
 let cardReady = new Card
 console.log(cardReady)
 let deckReady = new Deck
