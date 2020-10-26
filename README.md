d9f53d1b67fa30728c7afe0724bc65b331402fe8
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-10-26 18:19:47 -0400

    fixed console.log

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ Game of War is a card game where the objective is to acquire all of the cards, w
 
 ## Description 
 
-To start the game, you need to open your console. The game is set up to play with two players. The card deck of 52 is shuffled and distributed evenly to each player, face down. Type 'newGame.compareCards()' in to your console. The game now starts. Each player puts down their first card with the value facing up. Whoever has the bigger valued card, takes all of the cards that were put down. If the cards are of equal value, war is declared. In war, each player will draw and put down four cards, facing the fourth card face up. Whoevers card is of higher value, takes all. If the cards are of equal value, war is declared again. The game is played until one player has all 52 cards.
+To start the game, you need to open your console. The game is set up to play with two players. The card deck of 52 is shuffled and distributed evenly to each player, face down. Type 'newGame.compareCards()' in to your console. The game now starts. Each player puts down their first card with the value facing up. Whoever has the bigger valued card, takes all of the cards that were put down. If the cards are of equal value, war is declared. In war, each player will draw and put down four cards, facing the fourth card face up. Whoevers card is of higher value, takes all. If the cards are of equal value, war is declared again. The game is played until one player has all 52 cards. Your cards will be logged to the screen as Player Two or p2. This way, you can see each round you won, when war is declared, and who won each round. 
 
 ### Installing
 
-You will need chrome opened up so you can play out of your console. 
+You will need chrome opened up so you can play out of your console. Once chrome is open, open your developer tools by right clicking then clicking inspect. You will then click on the console tab. To begin the game, type everything in the quotes 'newGame.compareCards()'. You will run this command for each turn to play the game!
+
 
 ## Authors
 
--- game.js
+++ game.js
@@ -57,8 +57,7 @@ class War {
         
         this.playerOne.playerDeck = deck.splice(0, half)
         this.playerTwo.playerDeck = deck.splice(-half)
-        // console.log(this.playerOne.playerDeck)
-        // console.log(this.playerTwo.playerDeck)
+      
        
      }
     compareCards() {
@@ -72,10 +71,12 @@ class War {
             this.playerTwo.cardsInPlay.unshift(this.playerTwo.playerDeck.shift())
             
             if (this.playerOne.cardsInPlay[0].score > this.playerTwo.cardsInPlay[0].score) {
+                console.log('Player one wins this round', `p1: ${this.playerOne.cardsInPlay[0].score}`, `p2: ${this.playerTwo.cardsInPlay[0].score}`)
                 this.playerOne.playerDeck.push(this.playerOne.cardsInPlay.shift())
                 this.playerOne.playerDeck.push(this.playerTwo.cardsInPlay.shift())
                 console.log(this.playerOne.playerDeck, this.playerTwo.playerDeck)
             } else if (this.playerOne.cardsInPlay[0].score < this.playerTwo.cardsInPlay[0].score) {
+                console.log('Player two wins this round', `p1: ${this.playerOne.cardsInPlay[0].score}`, `p2: ${this.playerTwo.cardsInPlay[0].score}`)
                 this.playerTwo.playerDeck.push(this.playerTwo.cardsInPlay.shift())
                 this.playerTwo.playerDeck.push(this.playerOne.cardsInPlay.shift())
                 console.log(this.playerOne.playerDeck, this.playerTwo.playerDeck)
@@ -95,12 +96,16 @@ war() {
     }
 
     else {
+        console.log('War is happening')
         this.playerOne.cardsInPlay.unshift(this.playerOne.playerDeck.splice(0, 4))
         this.playerTwo.cardsInPlay.unshift(this.playerTwo.playerDeck.splice(0, 4))
         if (this.playerOne.cardsInPlay[0].score > this.playerTwo.cardsInPlay[0].score) {
+            console.log('Player one wins this round of war', `p1: ${this.playerOne.cardsInPlay[0].score}`, `p2: ${this.playerTwo.cardsInPlay[0].score}`)
             this.playerOne.playerDeck.push(this.playerOne.cardsInPlay.shift())
             this.playerOne.playerDeck.push(this.playerTwo.cardsInPlay.shift())
+
         } else if (this.playerOne.cardsInPlay[0].score < this.playerTwo.cardsInPlay[0].score) {
+            console.log('Player two wins this round of war', `p1: ${this.playerOne.cardsInPlay[0].score}`, `p2: ${this.playerTwo.cardsInPlay[0].score}`)
             this.playerTwo.playerDeck.push(this.playerTwo.cardsInPlay.shift())
             this.playerTwo.playerDeck.push(this.playerOne.cardsInPlay.shift())
         } else {
